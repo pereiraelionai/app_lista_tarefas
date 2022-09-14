c593435d25270beb331ec5685d1ce231bd394a50
pereiraelionai <pereira.elionai92@gmail.com>
2022-09-13 22:26:43 -0300

    finalizando aplicação, aplicando as funcionalidades na página index

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+	$acao = 'recuperarPendente';
+	require "tarefa_controller.php";
+?>
+
+
 <html>
 	<head>
 		<meta charset="utf-8" />
@@ -7,6 +13,55 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<script>
+			function editarPendente(id, txt_tarefa) {
+				
+				//criar um form para edição
+				let form = document.createElement('form')
+				form.action = 'tarefa_controller.php?acao=atualizarPendente'
+				form.method = 'post'
+				form.className = 'row'
+
+				let inputTarefa = document.createElement('input')
+				inputTarefa.type = 'text'
+				inputTarefa.name = 'tarefa'
+				inputTarefa.className = 'col-9 form-control'
+				inputTarefa.value = txt_tarefa
+
+				//input hidden que guarda o id da tarefa
+				let inputId = document.createElement('input')
+				inputId.type = 'hidden'
+				inputId.name = 'id'
+				inputId.value = id
+
+				let button = document.createElement('button')
+				button.type = 'submit'
+				button.className = 'btn btn-info'
+				button.innerHTML = 'Atualizar'
+
+				form.appendChild(inputTarefa)
+				form.appendChild(button)
+				form.appendChild(inputId)
+
+				//selecionar a div tarefa
+				let tarefa = document.getElementById('tarefa_' + id)
+				tarefa.innerHTML = ''
+
+				tarefa.insertBefore(form, tarefa[0])
+
+			}
+
+
+			function marcaRealizadaPendete(id) {
+				location.href = 'todas_tarefas.php?acao=marcarRealizadaPendente&id=' + id
+			}
+
+			function deletarPendete(id) {
+				location.href = 'todas_tarefas.php?acao=removerPendente&id=' + id
+			}
+		</script>
+
 	</head>
 
 	<body>
@@ -34,25 +89,27 @@
 						<div class="row">
 							<div class="col">
 								<h4>Tarefas pendentes</h4>
-								<hr />
+								<hr />	
+
+								<?php
+									
+									foreach($tarefas as $indice => $tarefa) {
+										if ($tarefa->id_status == 1) {
+								?>
 
 								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro</div>
+									<div id="tarefa_<?= $tarefa->id?>" class="col-sm-9"><?= $tarefa->tarefa?></div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
+										<i onclick="deletarPendete(<?= $tarefa->id ?>)" style="cursor: pointer;"  class="fas fa-trash-alt fa-lg text-danger"></i>
+										<i onclick="editarPendente(<?= $tarefa->id ?>, '<?= $tarefa->tarefa?>')" style="cursor: pointer;"  class="fas fa-edit fa-lg text-info"></i>
+										<i onclick="marcaRealizadaPendete(<?= $tarefa->id ?>)" style="cursor: pointer;"  class="fas fa-check-square fa-lg text-success"></i>
 									</div>
 								</div>
+								
+								<?php
+										}}
+								?>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
 							</div>
 						</div>
 					</div>
